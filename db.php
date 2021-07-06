e26b703277608e55f67cd9304a39146c7ab70da0
S-Mlangeni <smlangeni21@gmail.com>
2021-07-06 19:17:44 +0200

    update

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php 
-    //Loading php dotenv file:
-    
-    //
+    /*PHP Dotenv is meant for the development environment and
+    requires the .env file. However, since this file is ignored
+    in for production and environment variables are set in the
+    production environment, the file with dev env. variables is 
+    loaded conditionally as illustrated below while the production 
+    code runs on the prod. env. variables when file is ignored. */
+    //Loading php dotenv file for development environment:
     if (file_exists(".env")) {
         require("vendor/autoload.php");
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         $dotenv->load();
     }
-    //
     
     //Connecting to database:
     $connection = mysqli_connect(
